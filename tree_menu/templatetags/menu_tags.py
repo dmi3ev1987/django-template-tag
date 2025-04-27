d707d27c7ebaf6b1f530004af9e4c37b0f5cfc2c
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2025-04-28 02:22:46 +0300

    Refactor draw menu.

--- tree_menu/templatetags/menu_tags.py
+++ tree_menu/templatetags/menu_tags.py
@@ -1,25 +1,27 @@
 from django import template
-from django.utils.html import escape
-from ..models import MenuItem
-from ..utils import build_menu_tree, render_menu
+from django.template.loader import get_template
+
+from tree_menu.models import MenuItem
+from tree_menu.utils import build_menu_tree
 
 register = template.Library()
+
 
 @register.simple_tag(takes_context=True)
 def draw_menu(context, menu_name):
     request = context.get('request')
     if not request:
         return ''
-    
+
     current_path = request.path_info
-    
+
     try:
         menu_items = MenuItem.objects.filter(
-            menu_name=menu_name
+            menu_name=menu_name,
         ).select_related('parent')
-        
+
         items_by_parent = build_menu_tree(menu_items)
-        
+
         active_items = set()
         for item in menu_items:
             if item.is_active(current_path):
@@ -28,14 +30,15 @@ def draw_menu(context, menu_name):
                 while parent:
                     active_items.add(parent.id)
                     parent = parent.parent
-        
-        return render_menu(
-            items_by_parent=items_by_parent,
-            active_items=active_items,
-            current_path=current_path
-        )
-        
-    except Exception as e:
-        if settings.DEBUG:
-            raise e
+
+        template_name = 'tree_menu/menu.html'
+        template = get_template(template_name)
+        context = {
+            'items_by_parent': items_by_parent,
+            'active_items': active_items,
+            'current_path': current_path,
+        }
+        return template.render(context)
+    # Error handling - maybe need to add error message
+    except ValueError:
         return ''
