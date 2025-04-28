6824c153e11575ff8cd0d85ac6a5b276ae584b41
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2025-04-28 03:35:19 +0300

    Add menu example url.

--- template_tag/urls.py
+++ template_tag/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from tree_menu.views import menu_example
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', menu_example, name='menu-example'),
 ]
